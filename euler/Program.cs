0e5873a9e31db07ee9e630d82ce95f9feaa309f6
Simpiwe Sobici <G17S9627@ict.ru.ac.za>
2020-02-17 12:38:30 +0200

    Implemented highest prime factor problem

--- euler/Program.cs
+++ euler/Program.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace euler
 {
+
     class Program
     {
+        static int LargestPrimeFactor(int number)  
+        {  
+            #region Get prime factors
+            
+            List<int> primes = new List<int>(); 
+            if (number % 2 == 0)
+                primes.Add(2);
+
+            //eliminate all even numbers
+            while (number % 2 == 0)  
+            {   
+                number = number/2;
+            }  
+        
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)  
+            {   
+                while (number % i == 0)  
+                {  
+                    primes.Add(number);
+                    number /= i; 
+                }  
+            }  
+        
+            if (number > 2)  
+                primes.Add(number);
+            
+            #endregion
+
+            //return the largest prime factor
+            return primes.Max();
+        } 
+    
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
